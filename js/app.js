cf844146c1a7a6a07d267d0dd512607c7b593c69
dora5n <Moe4dora5n@gmail.com>
2023-06-02 03:08:13 +0900

    sako

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// alert(1111);
+
+console.log(111);
+
+console.log("js");
+
+let num = 1;
+console.log(num,"num箱");
+
+console.log(2 + 3,"計算中");
+
+console.log("2 + 3","どうなる？");
+
+
+
+$(".test").on("click", function () {
+
+  $(this).html(num);
+});
